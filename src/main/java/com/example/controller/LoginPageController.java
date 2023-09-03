431a4469ead2deab069b134b518b13b452394601
thisara <thisarakavishka0@gmail.com>
2023-09-03 20:55:01 +0530

    Create MainDashBoard with simple login

---
+++ src/main/java/com/example/controller/LoginPageController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginPageController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUserNameOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/controller/MainDashBoardController.java
@@ -0,0 +1,48 @@
+package com.example.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainDashBoardController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXButton btnDashBoard;
+
+    @FXML
+    private JFXButton btnStudent;
+
+    @FXML
+    private JFXButton btnRoom;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    private AnchorPane changeRoot;
+
+    @FXML
+    void btnDashBoardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRoomOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event) {
+
+    }
+
+}
