824897b36c725be24fecfb08cacc6ee83ac2b10a
thisara <thisarakavishka0@gmail.com>
2023-09-06 00:39:21 +0530

    Add RoomBO.java in BOS package

---
+++ src/main/java/com/example/bo/bos/RoomBO.java
@@ -0,0 +1,19 @@
+package com.example.bo.bos;
+
+import com.example.dto.RoomDTO;
+
+import java.util.List;
+
+public interface RoomBO {
+    Boolean saveRoom(RoomDTO dto);
+
+    Boolean updateRoom(RoomDTO dto);
+
+    Boolean deleteRoom(String id);
+
+    String getLastId();
+
+    RoomDTO searchRoom(String text);
+
+    List<RoomDTO> getAllRooms();
+}
