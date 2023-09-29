b8124f8cbacecd994f62b8720a8540f674dfba80
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-29 19:17:18 +0530

    Refactor DashBoardFormController.java & DashBoardForm.fxml with style.css

--- src/main/java/com/example/controller/DashBoardFormController.java
+++ src/main/java/com/example/controller/DashBoardFormController.java
@@ -1,4 +1,65 @@
 package com.example.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
 public class DashBoardFormController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Pane changeRoot;
+
+    @FXML
+    private JFXButton btnDashBoard;
+
+    @FXML
+    private JFXButton btnStudents;
+
+    @FXML
+    private JFXButton btnRoom;
+
+    @FXML
+    private JFXButton btnKeyMoney;
+
+    @FXML
+    private JFXButton btnRegistration;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    void btnDashBoardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKeyMoneyOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegistrationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRoomOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentsOnAction(ActionEvent event) {
+
+    }
+
 }
