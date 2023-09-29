7c70d0f2900920be6be627cefca096aa03cf8ad0
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-29 13:40:24 +0530

    Update LoginForm.fxml & LoginFormController.java

--- src/main/java/com/example/controller/LoginFormController.java
+++ src/main/java/com/example/controller/LoginFormController.java
@@ -1,14 +1,66 @@
 package com.example.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class LoginFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginFormController implements Initializable {
     @FXML
-    private Label welcomeText;
+    public AnchorPane root;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        txtUserName.requestFocus();
+    }
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+        System.out.println("login button clicked");
+        Stage stage = (Stage) root.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/example/view/DashBoardForm.fxml"))));
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(KeyEvent keyEvent) {
+        if (!txtPassword.getText().isEmpty() && (keyEvent.getCode() == KeyCode.ENTER)) {
+            btnLogin.fire();
+        } else if (txtPassword.getText().isEmpty() && (keyEvent.getCode() == KeyCode.BACK_SPACE || keyEvent.getCode() == KeyCode.UP)) {
+            txtUserName.requestFocus();
+        }
+    }
+
+    @FXML
+    void txtUserNameOnAction(KeyEvent keyEvent) {
+        if (!txtUserName.getText().isEmpty() && (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.DOWN)) {
+            txtPassword.requestFocus();
+        }
     }
 }
