a2e816ab106b0b7e0511223fc9d2ef1eff11d619
thisarakavishka <thisarakavishka0@gmail.com>
2023-10-03 23:21:08 +0530

    Refactor DashBoardFormController.java

--- src/main/java/com/example/controller/DashBoardFormController.java
+++ src/main/java/com/example/controller/DashBoardFormController.java
@@ -1,12 +1,21 @@
 package com.example.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class DashBoardFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashBoardFormController implements Initializable {
 
     @FXML
     private AnchorPane root;
@@ -32,34 +41,70 @@ public class DashBoardFormController {
     @FXML
     private JFXButton btnLogout;
 
+    private Stage stage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> {
+            stage = (Stage) root.getScene().getWindow();
+        });
+    }
+
     @FXML
     void btnDashBoardOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/example/view/DashBoardForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnKeyMoneyOnAction(ActionEvent event) {
-
+        try {
+            changeRoot.getChildren().clear();
+            changeRoot.getChildren().add(FXMLLoader.load(getClass().getResource("/com/example/view/KeyMoneyForm.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnLogoutOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/example/view/LoginForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnRegistrationOnAction(ActionEvent event) {
-
+        try {
+            changeRoot.getChildren().clear();
+            changeRoot.getChildren().add(FXMLLoader.load(getClass().getResource("/com/example/view/RegistrationForm.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnRoomOnAction(ActionEvent event) {
-
+        try {
+            changeRoot.getChildren().clear();
+            changeRoot.getChildren().add(FXMLLoader.load(getClass().getResource("/com/example/view/RoomsFrom.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnStudentsOnAction(ActionEvent event) {
-
+        try {
+            changeRoot.getChildren().clear();
+            changeRoot.getChildren().add(FXMLLoader.load(getClass().getResource("/com/example/view/RoomsFrom.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
 }
